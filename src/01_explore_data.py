f487ea7c3f6bdfa37687b7e0d732cdd6014442d1
Alba <albamartinglez@hotmail.com>
2026-05-18 18:59:17 +0200

    feat: add scripts for initial dataset exploration

---
+++ src/01_explore_data.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+
+df = pd.read_csv('data/train.csv')
+
+print("Dataset shape (rows, columns):")
+print(df.shape)
+
+print(df.head())
+
+print(df.isnull().sum())
+
+print(df['is_there_an_emotion_directed_at_a_brand_or_product'].value_counts())
+
+
+
